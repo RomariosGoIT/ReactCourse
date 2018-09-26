12ec0705dc65ad144b44ae5f618373b15003c4b8
RomariosGoIT <romantgt@gmail.com>
2018-09-26 10:19:18 +0300

    commit

---
+++ burger-project/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavItems from '../NavigationItems/NavItems';
+import classes from './SideDrawer.css';
+
+
+
+const sideDrawer = (props) => {
+    return(
+        <div className={classes.SideDrawer}>
+            <Logo/>
+            <nav>
+                <NavItems/>
+            </nav>
+        </div>
+    );
+};
+
+export default sideDrawer;
