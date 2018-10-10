9e34ceed07703b88a1af720db4480580135743b2
Romarios <romantgt@gmail.com>
2018-10-10 20:59:52 +0300

    commit

--- burger-project/src/containers/Orders/Orders.js
+++ burger-project/src/containers/Orders/Orders.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
-import Order from '../../components/Order/Order'
+import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandle/withErrorHandle';
 
 class Orders extends Component {
+
+    state = {
+        order: [],
+        loading: true
+    }
+
+    componentDidMount () {
+        axios.get('/orders.json')
+        .then(res=>{
+            const fetchOrders = [];
+            for (let key in res.data) {
+                fetchOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            this.setState({loading: false, order: fetchOrders});
+        })
+        .catch(err=>{
+            this.setState({loading: false});
+        })
+    }   
+
     render () {
         return (
             <div>
-                 <Order/>
-                 <Order/> 
+                 {this.state.order.map(order =>(
+                     <Order 
+                     key={order.id}
+                     ingredients={order.ingredients}
+                     price={order.price} />
+                 ))}
             </div>              
         )
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
