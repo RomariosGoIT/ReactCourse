9a8b82156b98044d53f77439a2f08336711bb520
RomariosGoIT <romantgt@gmail.com>
2018-10-14 16:26:42 +0300

    commit

--- burger-project/src/containers/Checkout/ContactData/ContactData.js
+++ burger-project/src/containers/Checkout/ContactData/ContactData.js
@@ -7,32 +7,115 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            steet: '',
-            postalCode: ''
+        orderForm : {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your name...'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your street...'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Conde...'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    maxLength: 5,
+                    minLength: 5
+                },
+                valid: false
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Contry...'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-mail...'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastes', displayValue: 'Fastes'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: 'fastes',
+                validation: {
+                    required: true
+                },
+                valid: false
+            }
         },
         loading: false
+    }
+
+    checkValidity = (value, rules) => {
+        let isValid = true;
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length >= 5 && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length <= 5 && isValid;
+        }
+
+        return isValid;
     }
 
     orderHandler = (event) => {
         event.preventDefault();
         this.setState({loading: true});
-        
+        const formData = {};
+        for (let formElement in this.state.orderForm) {
+            formData[formElement] = this.state.orderForm[formElement].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.totalPrice,
-            customer: {
-                name: 'Romarios',
-                address:{ 
-                street: 'Perova 1',
-                zipCode: '23453',
-                country: 'Kiev'
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: 'fastest'
+            orderData: formData
         }
         
         axios.post('/orders.json', order)
@@ -45,14 +128,39 @@ class ContactData extends Component {
         });
     }
 
+    inputChangeHandler = (event, elementId) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        const updatedValue = {
+            ...updatedOrderForm[elementId]
+        };
+        updatedValue.value = event.target.value;
+        updatedValue.valid = this.checkValidity(updatedValue.value, updatedValue.validation)
+        updatedOrderForm[elementId] = updatedValue;
+        console.log(updatedValue);
+        this.setState({orderForm: updatedOrderForm})
+    }
+
     render () {
+        const formElementArray = [];
+        for (let key in this.state.orderForm) {
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         let form = (
-            <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name.." />
-                <Input inputtype="input" type="email" name="email" placeholder="Your Email.." />
-                <Input inputtype="input" type="text" name="steet" placeholder="Steet.." />
-                <Input inputtype="input" type="text" name="postalCode" placeholder="PostalCode.." />
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            <form onSubmit={this.orderHandler}>
+                {formElementArray.map(fromElement =>(
+                    <Input
+                        key={fromElement.id} 
+                        elementType={fromElement.config.elementType}
+                        elementConfig={fromElement.config.elementConfig}
+                        value={fromElement.config.value}
+                        changed={(event) => this.inputChangeHandler(event, fromElement.id)} />
+                ))}
+                <Button btnType="Success">ORDER</Button>
             </form>
         );
         if (this.state.loading) {
