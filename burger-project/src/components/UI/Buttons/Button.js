95659e3155e87b530663bf458eb35cbd4e20f44c
RomariosGoIT <romantgt@gmail.com>
2018-10-17 11:04:43 +0300

    commit

--- burger-project/src/components/UI/Buttons/Button.js
+++ burger-project/src/components/UI/Buttons/Button.js
@@ -4,7 +4,7 @@ import classes from './Button.css'
 const button = (props) => (
     <button
         onClick={props.clicked}
-        disabled={props.disabled}
+        disabled={props.disabled}   
         className={[classes.Button, classes[props.btnType]].join(' ')} >
         {props.children}
     </button>
