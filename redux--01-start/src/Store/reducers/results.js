3b7d62377526c26691f8aa2526093ff8722a8a03
Romarios <romantgt@gmail.com>
2018-10-29 07:54:13 +0200

    commit

--- redux--01-start/src/Store/reducers/results.js
+++ redux--01-start/src/Store/reducers/results.js
@@ -5,15 +5,17 @@ const initialState = {
     results: []
 }
 
+const deleteResult =(state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updatedObject(state, {results: updatedArray});
+};
+
 const reducer = (state = initialState, action) => {
 
     switch(action.type) {
        
-        case actionTypes.STORE_RESULT:
-            return updatedObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
-        case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updatedObject(state, {results: updatedArray});
+        case actionTypes.STORE_RESULT : return updatedObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
+        case actionTypes.DELETE_RESULT : return deleteResult(state, action);
         default:
             return state
         
