32e3698f995a8a2aa3842060175546457c4d8022
RomariosGoIT <romantgt@gmail.com>
2018-11-15 14:49:01 +0200

    burger testing

--- burger-project/src/components/Navigation/NavigationItems/NavItems.test.js
+++ burger-project/src/components/Navigation/NavigationItems/NavItems.test.js
@@ -24,4 +24,10 @@ describe('<NavigationItems />', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
+    it('should an exact logout button', () => {
+        wrapper.setProps({isAuthenticate: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
+
+
 });
