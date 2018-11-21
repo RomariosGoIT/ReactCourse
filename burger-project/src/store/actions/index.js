7dc93514fd6423f793cc174b41dc209954e8c8ea
RomariosGoIT <romantgt@gmail.com>
2018-11-21 14:31:34 +0200

    saga manage

--- burger-project/src/store/actions/index.js
+++ burger-project/src/store/actions/index.js
@@ -15,5 +15,10 @@ export {
     auth,
     logout,
     setAuthRedirectPath,
-    authCheckState
+    authCheckState,
+    logoutSucceed,
+    authStart,
+    authSuccess,
+    checkAuthTimeout,
+    authFail
 } from './auth';
